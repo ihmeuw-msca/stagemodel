8671c09663ef549285c41f58cdf18e65a34991ff
zhengp0 <zhengp@uw.edu>
2021-01-04 11:02:20 -0800

    add badges from github workflow

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     sys.path.insert(0, src_dir.as_posix())
     import __about__ as about
 
-    with (base_dir/'README.md').open() as f:
+    with (base_dir/'README.rst').open() as f:
         long_description = f.read()
 
     install_requirements = [
