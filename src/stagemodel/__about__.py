8523d1eece1a3622db030fcd6667b24a8b71118d
zhengp0 <zhengp@uw.edu>
2021-06-22 10:36:36 -0700

    advance to version 0.0.4

--- src/stagemodel/__about__.py
+++ src/stagemodel/__about__.py
@@ -7,10 +7,10 @@ __title__ = "stagemodel"
 __summary__ = "stagemodel: Stagewise models."
 __uri__ = "https://github.com/ihmeuw-msca/stagemodel"
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 __author__ = "IHME Math Sciences"
 __email__ = "ihme.math.sciences@gmail.com"
 
 __license__ = "BSD-2-Clause License"
-__copyright__ = f"Copyright 2020 {__author__}"
+__copyright__ = f"Copyright 2021 {__author__}"
